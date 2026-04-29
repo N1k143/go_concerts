6e525d83ed19195ad865068c0b03f2c30531a3d4
N1k143 <nikitapetrunin296@gmail.com>
2026-04-29 09:24:32 +0500

    chore: project structure setup

---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	ServerPort string
+}
+
+func Load() *Config {
+	_ = godotenv.Load()
+
+	return &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
+		DBName:     getEnv("DB_NAME", "concerts"),
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+	}
+}
+
+func getEnv(key, defaultVal string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return defaultVal
+}
